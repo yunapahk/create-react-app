85191aef12d676374fbf801987a34b69204bd757
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-07-17 21:03:36 -0700

    Github button added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Main() {
-    return <h1>Main</h1>
-}
+    const redirectToGitHub = () => {
+      window.location.href = 'https://github.com/yunapahk';
+    };
+  
+    return (
+      <div>
+        <button className="github-button" onClick={redirectToGitHub}>
+          <FontAwesomeIcon icon={faGithub} style={{ color: "#621fa9" }} />
+        </button>
+      </div>
+    );
+  }
